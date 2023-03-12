0f7c422955cdb5e3c1d31c901eaf03ae0512c4a2
Joe Zhou <Joe.zhou266@gmail.com>
2023-03-12 13:19:46 -0400

    Update project to Chapter 9 version

--- microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/MessagingTests.java
+++ microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/MessagingTests.java
@@ -35,8 +35,10 @@ import static se.magnus.api.event.Event.Type.DELETE;
 import static se.magnus.microservices.composite.product.IsSameEvent.sameEventExceptCreatedAt;
 
 @SpringBootTest(
-        webEnvironment = RANDOM_PORT,
-        properties = {"spring.main.allow-bean-definition-overriding=true"})
+        webEnvironment = RANDOM_PORT, properties = {
+        "spring.main.allow-bean-definition-overriding=true",
+        "eureka.client.enabled=false"})
+
 @Import({TestChannelBinderConfiguration.class})
 class MessagingTests {
 
